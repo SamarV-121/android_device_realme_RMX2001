732017550044ddf586b3724366d379ab3ed7f576
Jens Lody <jens@jenslody.de>
2019-11-22 06:49:24 +0100

    RMX2001: recovery: Move fstab to symlinked folder
     Fixes the following build-error:
      cannot delete non-empty directory: root/etc
      could not make way for new symlink: root/etc
    
    Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>
    Change-Id: Ifd78aa570faeb195e3deaec867294553c24e67f2
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,7 +113,7 @@ TW_INCLUDE_FBE_METADATA_DECRYPT := true
 
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/etc/recovery.fstab
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_LARGE_FILESYSTEM := true
 
