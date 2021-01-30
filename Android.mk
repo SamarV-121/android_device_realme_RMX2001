54deb72a87f2bae323632bbc85a97b4e587224ba
TheMalachite <eliasgheeraert@gmail.com>
2021-01-30 13:16:32 +0100

    RMX2001: Initial bringup

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),rmx2020)
+ifeq ($(TARGET_DEVICE),RMX2001)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,6 +15,6 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_rmx2020.mk
+    $(LOCAL_DIR)/omni_RMX2001.mk
 
-COMMON_LUNCH_CHOICES := omni_rmx2020-eng
+COMMON_LUNCH_CHOICES := omni_RMX2001-eng
