85f4e03c78a26d8ca74d33a7b1278f826ac18927
SamarV-121 <samarvispute121@gmail.com>
2021-03-22 17:20:19 +0530

    RMX2001: Update vendor path
    Change-Id: Ic92bf2b8c75dbe634732e20c90d7af74941272c2
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- omni_RMX2001.mk
+++ omni_RMX2001.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our PitchBlack configuration
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
 
